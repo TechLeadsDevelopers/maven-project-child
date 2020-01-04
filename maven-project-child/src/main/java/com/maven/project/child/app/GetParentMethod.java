eb3259da6786b302a58ce0e0917a17e7a6cd745b
dell <anupoju.madhav@gmail.com>
2020-01-04 08:56:17 +0530

    This Maven child project derives jars and code
    From the parent maven project (maven-project-parent)

---
+++ maven-project-child/src/main/java/com/maven/project/child/app/GetParentMethod.java
@@ -0,0 +1,12 @@
+package com.maven.project.child.app;
+
+import com.maven.parent.app.ParentApp;
+
+public class GetParentMethod {
+
+	public static void main(String[] args) {
+		
+		ParentApp.loadMySQLDriver();
+	}
+
+}
